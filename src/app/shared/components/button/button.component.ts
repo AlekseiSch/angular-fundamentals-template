cf02b782f8fbb6cce3ed750589fa7487b95f59f4
AlekseiShcherbakov <Aleksei_Shcherbakov@epam.com>
2024-03-18 20:29:23 +0100

    task3

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -13,4 +14,12 @@ export class ButtonComponent {
   }
 
   // Use the names for the inputs `buttonText` and `iconName`.
+  @Input() type: string = "button";
+  @Input() buttonText: string = "";
+  @Input() iconName: IconDefinition | null = null;
+  @Output() onClick: EventEmitter<void> = new EventEmitter();
+
+  onClickHandler (e: MouseEvent) {
+    this.onClick.emit()
+  }
 }
